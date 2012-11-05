75b7b9271c0dc0702fca582ba088510f3f8a182d
Martin Blackburn <walking_fishy@hotmail.com>
2012-11-05 11:56:18 +0000

    tabs now working

--- tabs.js
+++ tabs.js
@@ -1,7 +1,7 @@
 Tabs = function(tabs) 
 {   
     //hide all but first tab
-    tabs.find(".tabContent").not(":first").toggle();
+    tabs.find(".tabContent").not(":first").css("display", "none");
     
     //add selected to first tab control
     tabs.find(".tabControls a").eq(0).addClass("selected");
@@ -11,7 +11,15 @@ Tabs = function(tabs)
     {
         event.preventDefault();
         
+        var link = $(this);
         
+        //remove selected class, hide tab content
+        tabs.find(".tabControls a").removeClass("selected");
+        tabs.find(".tabContent").css("display", "none");
+        
+        //added selected class, show tab content
+        link.addClass("selected");
+        $(link.attr("href")).css("display", "block");
     });
 };
 
